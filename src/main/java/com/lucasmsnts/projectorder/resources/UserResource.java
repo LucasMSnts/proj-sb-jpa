650e0bf5ae8397ec23c52a4ab4686ff6e6b5c65f
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-09-27 10:47:09 -0300

    User entity and resource

---
+++ src/main/java/com/lucasmsnts/projectorder/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.lucasmsnts.projectorder.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lucasmsnts.projectorder.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "João", "joao@gmail.com", "99995678", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
