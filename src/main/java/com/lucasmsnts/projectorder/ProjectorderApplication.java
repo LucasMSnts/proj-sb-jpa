c00e6563afd521226da469f6dd8f0fffc20caf67
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-09-27 10:02:20 -0300

    Project created

---
+++ src/main/java/com/lucasmsnts/projectorder/ProjectorderApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmsnts.projectorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectorderApplication.class, args);
+	}
+
+}
