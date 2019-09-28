05803df362cb74fa246aa2ad66ecd8af7cb26593
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-09-28 16:36:30 -0300

    Service layer, component registration

--- src/main/java/com/lucasmsnts/projectorder/repositories/UserRepository.java
+++ src/main/java/com/lucasmsnts/projectorder/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lucasmsnts.projectorder.entities.User;
 
+// @Repository não é necessario pois está herdando do JpaRepository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
