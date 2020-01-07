b23c7869269e2ee51634a768f7910baebd5ff7bf
Lucas Mauro <lucas_de_mauro@hotmail.com>
2020-01-07 16:36:26 -0300

    Exception handling - delete

--- src/main/java/com/lucasmsnts/projectorder/services/UserService.java
+++ src/main/java/com/lucasmsnts/projectorder/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.lucasmsnts.projectorder.entities.User;
 import com.lucasmsnts.projectorder.repositories.UserRepository;
+import com.lucasmsnts.projectorder.services.exceptions.DatabaseException;
 import com.lucasmsnts.projectorder.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) { 
+			// catch (RuntimeException e) // mostra todos os erros
+			//	e.printStackTrace(); // imprime no console os erros
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
